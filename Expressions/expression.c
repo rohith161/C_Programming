05be297c6104c30c58f8c40b381d2e4644cdbe04
rohith161 <rajrohith161@gmail.com>
2023-09-08 19:40:15 +0530

    Updated the expression

--- Expressions/expression.c
+++ Expressions/expression.c
@@ -1,9 +1,10 @@
 #include "stackImp.h"
 #include <string.h>
 
+// to check th whether the given expression has balanced brackets 
 int evaluatexp(char* str){
   Stack* stack = initStack();
-int count  = 0;
+  int count  = 0;
   for(int i=0; str[i] != '\0'; i++){
     char ch = str[i];
     if( (ch == '(' || ch == '{' || ch == '[' || ch == ')' || ch == '}' || ch == ']') ){
@@ -37,12 +38,13 @@ int count  = 0;
   } 
   
     printf("\n%s\n",(isEmpty(stack) ? "Correct Format" : "Wrong Format"));
-    int out = (isEmpty(stack)) ? 1 : 0;
+    int out = isEmpty(stack);
     clear(stack);
     return out;
 
 }
 
+// To assign the highest precendence according to the Operator
 int priority(const char c){
     if(c == '*'){
       return 4;
@@ -61,47 +63,47 @@ int priority(const char c){
     }
 }
 
-
+// conversion of the gien expression in the postfix state for better machine understanding
 char* postfix(char* s){
-  if(evaluatexp(s) == 1){
   Stack* stack = initStack();
   int size = strlen(s);
   char* res = (char*) malloc((size*2) * sizeof(char));
   int count = 0;
   for(int i=0; i<size; i++){
-    if(s[i] == ' '){
+    char ch = s[i];
+    if(ch == ' '){
       continue;
     }
-   // printf("%c ",Top(stack));
-    int precedence = priority(s[i]);
-    if(s[i] == '('){
-      push(stack,s[i]);
+    int precedence = priority(ch);
+    if(ch == '('){
+      push(stack,ch);
       continue;
     }
 
-    else if(s[i] >= '0' && s[i] <= '9'){
+    else if(ch >= '0' && ch <= '9'){
       if(res[count-2] >= '0' && res[count-2] <= '9' && s[i-1] >= '0' && s[i-1] <= '9' ){
-        res[count-1] = s[i];
+        res[count-1] = ch;
         res[count++] = ' ';
         continue;
       }
-      res[count++] = s[i];
+      res[count++] = ch;
       res[count++] = ' ';
       continue;
     }
 
-    else if( (Top(stack) == '('  || (precedence >= 1 && precedence <= 4) )){
+    else if( Top(stack) == '('  || (precedence >= 1 && precedence <= 4) ){
 
-      if(priority(Top(stack)) > precedence && precedence != 0){
-        while(!isEmpty){
+      if(priority(Top(stack)) > precedence){
+        while (!isEmpty(stack) && Top(stack) != '(')
+        {
           res[count++] = pop(stack);
           res[count++] = ' ';
-        }
-        push(stack,s[i]);
+        }  
+        push(stack,ch);
         continue;
       }
       if(precedence >= 1 && precedence <= 4){
-      push(stack,s[i]);
+      push(stack,ch);
       continue;
       }
     }
@@ -116,7 +118,7 @@ char* postfix(char* s){
 
       }
     }
-    else if( !isEmpty(stack) &&  (s[i] == ')' || (priority(Top(stack)) >= 1 &&  priority(Top(stack)) <= 4 )) ){
+    else if( !isEmpty(stack) &&  (ch == ')' || (priority(Top(stack)) >= 1 &&  priority(Top(stack)) <= 4 )) ){
       res[count++] = pop(stack);
       res[count++] = ' ';
       pop(stack);
@@ -134,13 +136,10 @@ char* postfix(char* s){
 
   clear(stack);
   return res;
-  }
-  else{
-    return "Invalid format";
-  }
 
 }
 
+// To perform the operator operation on the operands
 void execute(Stack* st,int op1,int op2,const char* token){
   if(*token == '*'){
     push(st,(op1 * op2));
@@ -163,11 +162,8 @@ void execute(Stack* st,int op1,int op2,const char* token){
     }
 }
 
+// Function which calculates the given expression
 int calculation(char* s){
-   if(evaluatexp(s) == 0){
-    printf("\n Invalid Format\n");
-    return -1;
-  }
   int out = 0;
   char* postfix_exp = postfix(s);
   printf("\n%s\n", postfix_exp);
@@ -175,7 +171,6 @@ int calculation(char* s){
   char* token =  strtok(postfix_exp," ");
   while (token != NULL)
   {
-   
     if(*token == '*' || *token == '/' || *token == '+' || *token == '-' ){
       int op2 = pop(st);
       int op1 = pop(st);
@@ -193,11 +188,12 @@ int calculation(char* s){
   clear(st);
   return out;
 }
+
 int main(){
     char str[] = "{[(12)(14)]}";
 
 
-    char infix[] = "1123+25*14/73";    // (((1123+25)*14)/73)    1123+25*14/73 
+    char infix[] = "{[(1123+25)*14]/73}  ";    // (((1123+25)*14)/73)    1123+25*14/73 
 
     evaluatexp(infix);
 
